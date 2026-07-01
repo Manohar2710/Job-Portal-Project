26d20d9bea34ff18afbb2268581fca03a6f08d2d
Manohar B <manohar1ga15ec430@gmail.com>
2026-07-01 14:54:29 +0530

    Feature/Springboot/142/Implement user Entity

--- Springboot/security-module/src/main/java/com/learning/security/dto/LoginRequest.java
+++ Springboot/security-module/src/main/java/com/learning/security/dto/LoginRequest.java
@@ -2,7 +2,6 @@ package com.learning.security.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
     @NotBlank(message = "email is required")
---
+++ Springboot/security-module/src/main/java/com/learning/security/entity/User.java
@@ -0,0 +1,39 @@
+package com.learning.security.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+    @UpdateTimestamp
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLogin;
+
+}
