0cf1f716b705a4f58b37a2f8f042b669819a2375
Manohar B <manohar1ga15ec430@gmail.com>
2026-07-01 09:43:08 +0530

    Feature/springboot/138/JWT Configuration properties closes #138

--- Springboot/security-module/src/main/java/com/learning/security/config/JwtProperties.java
+++ Springboot/security-module/src/main/java/com/learning/security/config/JwtProperties.java
@@ -1,13 +1,13 @@
 package com.learning.security.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 
 @Data
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "security.jwt")
 public class JwtProperties {
     private String secretKey;
