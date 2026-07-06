b098a0223d5dd54bccafd8764d6640d39af223df
Manohar B <manohar1ga15ec430@gmail.com>
2026-07-06 17:03:59 +0530

    Feature/Angular/180/auth-api library configuration for register and login API using openapi swagger code gen

---
+++ Angular/job-portal-workspace/libs/auth-api/model/models.ts
@@ -0,0 +1,6 @@
+export * from './authReponse';
+export * from './jobRequest';
+export * from './jobResponse';
+export * from './loginRequest';
+export * from './registerRequest';
+export * from './userInfo';
--- Angular/job-portal-workspace/projects/job-portal-app/src/app/pages/home/home.ts
+++ Angular/job-portal-workspace/projects/job-portal-app/src/app/pages/home/home.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthControllerService, LoginRequest } from 'auth-api';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+    constructor(private authApi: AuthControllerService){
+      
+    }
+    ngOnInit() {
+      console.log("App ngOninit")
+      let loginRequest : LoginRequest = {
+        email: "test@gmail.com",
+        password: "12345"
+      }
+      this.authApi.login(loginRequest).subscribe( (res) =>
+        console.log("response "+ res)
+      );
+    }
+}
