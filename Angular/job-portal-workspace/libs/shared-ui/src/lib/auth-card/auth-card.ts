96d25a8bf0dccef6fccdcee29570d279dc996f17
Manohar B <manohar1ga15ec430@gmail.com>
2026-08-06 22:24:39 +0530

    Feature/angular/259/Update-UI-screen-to-use-Angular-Materials

--- Angular/job-portal-workspace/libs/shared-ui/src/lib/auth-card/auth-card.ts
+++ Angular/job-portal-workspace/libs/shared-ui/src/lib/auth-card/auth-card.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'ui-auth-card',
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './auth-card.html',
   styleUrl: './auth-card.css',
 })
 export class AuthCard {
-
   @Input() title: string = '';
   @Input() subTitle: string = '';
 }
--- Angular/job-portal-workspace/libs/shared-ui/src/lib/form-field/form-field.ts
+++ Angular/job-portal-workspace/libs/shared-ui/src/lib/form-field/form-field.ts
@@ -1,29 +1,30 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'ui-form-field',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './form-field.html',
   styleUrl: './form-field.css',
 })
 export class FormField {
   @Input() label: string = '';
-  @Input() type: string = ''; // text | email | password
+  @Input() type: string = '';       // text | email | password
   @Input() placeholder: string = '';
   @Input() control!: AbstractControl;
 
-  // true when the field should show error state
   get hasError(): boolean {
     return !!this.control?.invalid && !!this.control?.touched;
-  } 
+  }
 
   get errorMessage(): string {
     const errors = this.control?.errors;
     if (!errors) return '';
-    if (errors['required']) return `${this.label} is required.`;
-    if (errors['email'])    return 'Please enter a valid email address.';
+    if (errors['required'])  return `${this.label} is required.`;
+    if (errors['email'])     return 'Please enter a valid email address.';
     if (errors['minlength']) {
       const min = errors['minlength'].requiredLength as number;
       return `Must be at least ${min} characters.`;
